76948ff8276bb67106a78ad17e7cb9be717fec64
64070014Fluk <64070014@it.kmitl.ac.th>
2023-10-11 05:50:33 +0700

    add following page

--- src/routes/feed/+page.svelte
+++ src/routes/feed/+page.svelte
@@ -3,11 +3,11 @@
   </script>
 
 <div class="h-full w-full p-2">
-	<div class="text-center my-6 font-bold">
-		<a href="/feed" class=" bg-[#F7B155] text-black">
+	<div class="flex justify-center text-center my-6 font-bold">
+		<a href="/feed" class=" bg-[#F7B155] text-black w-24 rounded-full mx-1">
 			For You
 		</a>
-		<a href="/feed">
+		<a href="/feed/following" class=" bg-trasparent text-white w-24 rounded-full mx-1">
 			Following
 		</a>
 	</div>
---
+++ src/routes/feed/following/+page.svelte
@@ -0,0 +1,12 @@
+<div class="h-full w-full p-2">
+	<div class="flex justify-center text-center my-6 font-bold">
+		<a href="/feed" class=" bg-trasparent text-white w-24 rounded-full mx-1">
+			For You
+		</a>
+		<a href="/feed/following" class=" bg-[#F7B155] text-black w-24 rounded-full mx-1">
+			Following
+		</a>
+	</div>
+
+
+</div> 
