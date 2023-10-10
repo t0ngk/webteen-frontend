27a4258aa2b86a3ad50b79458c9729e9881215d9
CHOMPOO (ชมพู่) <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-10-10 23:51:48 +0700

    Update search admin

--- src/lib/components/admin/Search.svelte
+++ src/lib/components/admin/Search.svelte
@@ -1,13 +1,59 @@
-<div><form>   
-    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
-        </div>
-        <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
-        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-    </div>
-</form>
+<div class="flex flex-row justify-between px-2">
+	<!-- Triple bar svg -->
+	<div>
+		<svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" viewBox="0 -10 60 60">
+			<rect width="40" height="4" rx="2.5" ry="2.5" fill="#98A2B3" />
+			<rect y="15" width="40" height="4" rx="2.5" ry="2.5" fill="#98A2B3" />
+			<rect y="30" width="40" height="4" rx="2.5" ry="2.5" fill="#98A2B3" />
+		</svg>
+	</div>
+	<!-- Search bar -->
+	<form>
+		<div class="relative">
+			<div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+				<svg
+					class="w-5 h-5 text-gray-500 dark:text-gray-400"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 20 20"
+				>
+					<path
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+					/>
+				</svg>
+			</div>
+			<input
+				type="search"
+				id="default-search"
+				class="block w-full p-2 pl-28 text-sm text-white border-['#344054'] bg-[#292932] rounded-lg"
+				placeholder="Search"
+				required
+			/>
+		</div>
+	</form>
+	<!-- Noti -->
+	<div class="relative mt-1">
+		<div
+			class="absolute bottom-auto left-auto right-0 top-0 inline-block -translate-y-1/2 translate-x-2/4 rotate-0 skew-x-0 skew-y-0 scale-x-50 scale-y-50 rounded-full bg-pink-700 p-2.5 text-xs"
+		/>
+		<div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor"
+				class="h-7 w-7"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		</div>
+	</div>
 </div>
