92bbcc10e3c2972cb116c3a562a1dec8fb2042be
64070014Fluk <64070014@it.kmitl.ac.th>
2023-10-10 23:41:40 +0700

    add login & signup

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+    let yes = false;
+</script>
+
+<div class="flex justify-center text-center items-center h-[95%] p-2">
+    <form class="flex flex-col space-y-5 w-10/12" action="/">
+        <h1 class="flex text-2xl mt-6 text-white font-bold">Log in</h1>
+        
+        <input class="shadow appearance-none border bg-transparent border-[#B4B4B4] rounded-3xl w-full p-3 text-[#B4B4B4] text-xs font-semibold leading-tight focus:outline-none focus:shadow-outline"
+        name="email" 
+        placeholder="Email / Username" 
+        required
+        type="email"
+        
+        />
+        <input class="shadow appearance-none border bg-transparent border-[#B4B4B4] rounded-3xl w-full p-3 text-[#B4B4B4] text-xs font-semibold leading-tight focus:outline-none focus:shadow-outline"
+        name="password"
+        placeholder="Password"
+        type="password"
+        required
+        />
+
+        <div class="flex font-medium text-sm px-2">
+            <input class="mr-2  bg-transparent checked:bg-yellow-500" 
+                type="checkbox" bind:checked={yes} />
+            <p class="">
+                Remember me
+            </p>
+            <a href="/login" class="ml-auto text-[#F7B155] hover:underline dark:text-yellow-600">Forgot password ?</a>
+        </div>
+
+        <div></div>
+        <button class=" text-black bg-[#F7B155] h-10 rounded-3xl font-bold text-sm tracking-wide disabled:bg-[#838383]"
+        disabled={!yes} on:click={() => console.log("login")}>LOGIN</button>
+
+    </form>
+</div>
+<div class="text-center font-medium text-sm mb-5">
+    <p class="text-white">
+        Don't have an account ? 
+        <a href="/signup" class="ml-auto text-[#F7B155] hover:underline dark:text-yellow-600">Sign up</a>
+    </p>
+</div>
