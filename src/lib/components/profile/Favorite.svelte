1144eaa5d76043428b8ca7c1b415b56c43cbc89b
llPornpilin <linglingppl@gmail.com>
2023-10-12 00:05:57 +0700

    profile nav bar

---
+++ src/lib/components/profile/Favorite.svelte
@@ -0,0 +1,3 @@
+<div class="h-full w-full p-2">
+    <h1 class="text-xl">Favorite</h1>
+</div>
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,3 +1,28 @@
+<script>
+	import HeaderProfile from '$lib/components/profile/headerProfile.svelte';
+	import Icon from '@iconify/svelte';
+	// import walletLinear from '@iconify/icons-solar/wallet-linear';
+</script>
+
 <div class="h-full w-full p-2">
-	<h1 class="text-5xl">Profile</h1>
+	<div class="flex pl-3 pt-3 justify-between pr-3">
+		<div class="flex flex-row">
+			<div class="h-[43px] w-[43px] rounded-full border-solid border-[2.5px] border-amber-300"></div>
+			<div class="pl-3">
+				<p class="flex">
+					<b>username</b>
+					<Icon icon="mdi:pencil-outline" color="white" class="h-[25px] w-[25px] pl-2 " />
+				</p>
+				<p>Level : Royal</p>
+			</div>
+		</div>
+		<div class="flex flex-row items-center">
+			<Icon icon="circum:bitcoin" color="#f7b155" class="h-[30px] w-[30px]" />
+			<h3 class="pl-1"><b>9999</b></h3>
+		</div>
+		
+	</div>
+	<div class="h-[115px]">
+		<HeaderProfile />
+	</div>
 </div>
