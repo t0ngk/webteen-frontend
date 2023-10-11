af10f65b37bf6319b4c8578254d374638e9057eb
PR800 <64070025@it.kmitl.ac.th>
2023-10-12 00:30:38 +0700

    Sticky Writing&Category header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -42,25 +42,27 @@
 	let active = nav === '/writing' ? navWriting[0] : navCategory[0];
 </script>
 
-<div>
-	<div class="p-2">
-		<h1 class="text-2xl font-semibold tracking-[1px]">
-			{nav == '/writing' ? 'WRITING' : 'CATEGORY'}
-		</h1>
-	</div>
-	<div class="flex flex-row justify-between border-b-[1px] border-[#373739a8] px-2">
-		{#each nav === '/writing' ? navWriting : navCategory as item}
-			<div>
-				<button
-					on:click={() => {
-						active = item;
-					}}
-					class={` py-1 px-2 text-base font-medium ${
-						active == item ? 'border-[#F7B155] text-[#F7B155] border-b-[2px]' : 'border-b-[0px] text-[#979797]'
-					}`}>{item.name}</button
-				>
-			</div>
-		{/each}
+<div >
+	<div class="sticky top-0 pt-4 bg-[#161218]">
+		<div class="p-2 realtive">
+			<h1 class="text-2xl font-semibold tracking-[1px]">
+				{nav == '/writing' ? 'WRITING' : 'CATEGORY'}
+			</h1>
+		</div>
+		<div class="flex flex-row justify-between border-b-[1px] border-[#373739a8] px-2">
+			{#each nav === '/writing' ? navWriting : navCategory as item}
+				<div>
+					<button
+						on:click={() => {
+							active = item;
+						}}
+						class={` py-1 px-2 text-base font-medium ${
+							active == item ? 'border-[#F7B155] text-[#F7B155] border-b-[2px]' : 'border-b-[0px] text-[#979797]'
+						}`}>{item.name}</button
+					>
+				</div>
+			{/each}
+		</div>
 	</div>
 	<svelte:component this={active.component} />
 </div>
--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -1,17 +1,21 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
 	import Icon from '@iconify/svelte';
+	import { goto } from '$app/navigation';
 </script>
 
-<div class="h-full w-full p-2 py-4">
-	<div class="w-10 h-10 bg-[#373739] rounded-full right-4 top-4"
-		style="position: absolute; "
-	/>
-	<Icon icon="la:wallet" color="#f7b155" 
-		style="position: absolute; font-size: 24;"
-		class="right-6 top-6"
-	/>
+<div class=" h-full w-full px-2">
 	<div class="h-[115px]">
 		<Header />
+		<button
+			on:click={() => {
+				goto("/income");
+			}}
+			class="absolute w-10 h-10 right-0 top-0"
+		>
+			<div class="absolute w-10 h-10 bg-[#373739] rounded-full right-4 top-4"
+			/>
+			<Icon icon="la:wallet" color="#f7b155" class="absolute right-6 top-6 text-[24px]"/>
+		</button>
 	</div>
 </div>
