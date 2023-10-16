52bfb8994049fac6501b3f7fa55d15f15044143b
Tadporn <88502892+Tadphorn@users.noreply.github.com>
2023-10-16 22:48:15 +0700

    detailEp page

---
+++ src/lib/components/book/CommentBook.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Icon from '@iconify/svelte';
+</script>
+
+<div class="w-full h-32 mb-4">
+	<div class="flex flex-row items-center">
+		<div class="w-8 h-8 bg-gray-200 rounded-full" />
+		<h1 class="text-sm font-semibold ml-2">User1</h1>
+		<Icon icon="la:award" color="white" style="font-size: 25px" class="ml-2" />
+		<p class="text-sm text-gray-400 ml-1 flex-grow">3 Feb 2023</p>
+		<div class="items-center mt-4">
+			<Icon icon="ph:dot-bold" color="white" style="font-size: 25px" class="ml-2 -mt-5" />
+			<Icon icon="ph:dot-bold" color="white" style="font-size: 25px" class="ml-2 -mt-5" />
+			<Icon icon="ph:dot-bold" color="white" style="font-size: 25px" class="ml-2 -mt-5" />
+		</div>
+	</div>
+	<p class="line-clamp-2 ml-8 text-sm mt-1 pr-3">
+		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque mollitia cumque quibusdam
+		similique error fugit eveniet non. Ab, nihil, perspiciatis inventore quibusdam, deleniti quos
+		quaerat iusto ipsa necessitatibus eius qui!
+	</p>
+	<div class="flex flex-row items-center pr-3 mt-4">
+		<p class="text-sm text-gray-400 ml-8 flex-grow">5 reply</p>
+		<Icon icon="iconamoon:like" color="white" style="font-size: 20px" class="" />
+		<p class="text-sm ml-1">621</p>
+		<Icon icon="iconamoon:like" color="white" style="font-size: 20px" class="ml-1" />
+		<p class="text-sm ml-1">4</p>
+	</div>
+	<div class="border border-white opacity-40 my-3" />
+</div>
---
+++ src/routes/category/detailBook/detailEP/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+	import CommentBook from '$lib/components/book/CommentBook.svelte';
+	import Recommend from '$lib/components/book/Recommend.svelte';
+
+	import Icon from '@iconify/svelte';
+</script>
+
+<div class="w-full h-full">
+	<div class="w-full bg-black h-12 flex pl-3 items-center">
+		<Icon icon="uiw:menu" color="white" style="font-size: 25px" />
+	</div>
+	<div class="w-full h-5/6 bg-gray-300">content</div>
+	<!-- navigate under content -->
+	<div class="w-full h-10 bg-gray-700 flex items-center pl-3">
+		<div class="flex flex-row flex-grow">
+			<Icon icon="ph:heart-bold" color="white" style="font-size: 20px" />
+			<p style="font-size: 14px" class="ml-1">3.2 k</p>
+			<Icon icon="gg:comment" color="white" style="font-size: 20px" class="ml-2" />
+			<p style="font-size: 14px" class="ml-1">30</p>
+		</div>
+		<div class="flex flex-row items-center">
+			<p>#98</p>
+			<Icon icon="pajamas:chevron-right" color="white" style="font-size: 35px" />
+		</div>
+	</div>
+	<div class="pl-3">
+		<h1 class="text-xl font-medium mt-4">Recommend</h1>
+		<div class="flex flex-row gap-1 mt-3">
+			<Recommend />
+		</div>
+		<!-- comment -->
+		<h1 class="text-xl font-medium mt-4">Top comment</h1>
+		<div class="p-3 mt-4">
+			<CommentBook />
+			<CommentBook />
+			<CommentBook />
+		</div>
+	</div>
+</div>
