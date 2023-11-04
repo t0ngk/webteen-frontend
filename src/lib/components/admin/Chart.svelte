95a35e922e3b0a32b35554b23f8e8325dcc1f0e3
CHOMPOO (ชมพู่) <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-11-04 12:13:56 +0700

    เพิ่มกราฟ

--- src/lib/components/admin/Chart.svelte
+++ src/lib/components/admin/Chart.svelte
@@ -1,9 +1,34 @@
-<div class="bg-[#292932] mx-1 my-5">
-    content content content<br>
-    content content content<br>
-    content content content<br>
-    content content content<br>
-    content content content<br>
-    content content content<br>
-    content content content<br>
-</div>
+<script>
+    import { chart } from 'svelte-apexcharts?client';
+  
+    let categories = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Nov', 'Dec']
+  
+    let sumMonthChart = {
+      chart: {
+        type: 'bar'
+      },
+      series: [
+        {
+          name: 'follower',
+          data: categories.map(() => Math.floor(Math.random() * 100))
+        }
+      ],
+      xaxis: {
+        categories
+      },
+      fill: {
+        colors: ['#F7B155']
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 10
+        }
+      }
+    };
+  </script>
+  
+    <p>User Chart</p>
+  <div class="w-full text-black">
+    <div class="w-full" use:chart={sumMonthChart} />
+  </div>
+  
