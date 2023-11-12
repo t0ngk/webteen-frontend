ea6e7c50dea15ea2b092ef6aa6df2d61f58bc24e
llPornpilin <linglingppl@gmail.com>
2023-11-13 06:46:23 +0700

    fetch data History

--- src/lib/components/book/RecentTemplate.svelte
+++ src/lib/components/book/RecentTemplate.svelte
@@ -1,52 +1,63 @@
 <script>
     import Icon from '@iconify/svelte';
+    import { onMount } from 'svelte';
+    import axios from 'axios';
 
-    const bookExamples = [
-		{
-			name: 'No Name 404',
-            author: 'A',
-            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN-TGUnYCYmr-IVYQjN9DgneizSMZJUob733p-6Vw3ByB9QuSTaLYOSLkvRz9VSw75vQ&usqp=CAU',
-		},
-		{
-			name: 'อ่านฉันสิ',
-            author: 'B',
-            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn7BZnI7PbKGdUHaL0GkSyNdj530Gvhde3ZdLwxXPF6gWUSefNsxbTl8PElwmNcELou8s&usqp=CAU',
-		},
-		{
-			name: 'กระต่ายกับเต่า',
-            author: 'C',
-            image: 'https://i.pinimg.com/originals/fc/7c/8b/fc7c8bd8f242e2765dfd99b619002712.jpg',
-		},
-		{
-			name: 'กระต่ายกับเต่า',
-            author: 'C',
-            image: 'https://i.pinimg.com/originals/fc/7c/8b/fc7c8bd8f242e2765dfd99b619002712.jpg',
-		},
-		{
-			name: 'กระต่ายกับเต่า2',
-            author: 'C',
-            image: 'https://i.pinimg.com/originals/fc/7c/8b/fc7c8bd8f242e2765dfd99b619002712.jpg',
-		},
-		{
-			name: 'กระต่ายกับเต่า3',
-            author: 'C',
-            image: 'https://i.pinimg.com/originals/fc/7c/8b/fc7c8bd8f242e2765dfd99b619002712.jpg',
-		},
-	];
+    let recentRead = [];
+    let historyBooks = [];
+    let isDataLoad = false;
+    let isDataLoaded = false;
+
+
+    const getHistory = async () => {
+		try {
+			const res = await axios.get(
+				`http://localhost:8082/history-service/getHistory`
+			);
+			historyBooks = res.data;
+			isDataLoad = true; // Set the flag to true once data is loaded
+
+            await getChapterHistory();
+		} catch (error) {
+			console.error('Error fetching books:', error);
+		}
+	};
+    const getChapterHistory = async () => {
+        try {
+            const bookPromises = historyBooks.map(async (history) => {
+                const chapterId = history.chapterId;
+                const bookRes = await axios.get(`http://localhost:8082/history-service/getChapter/${chapterId}`);
+                return bookRes.data;
+            });
+
+            const booksData = await Promise.all(bookPromises);
+            console.log(">>>>>>>>>>>>>>> ", booksData)
+
+            recentRead = booksData;
+            isDataLoaded = true;
+            console.log("(((((( ", recentRead);
+        } catch (error) {
+            console.error('Error fetching Books History:', error);
+        }
+    };
+    onMount(getHistory);
+
+
 </script>
 
 <div class="mt-5">
-    {#each bookExamples as book}
+    {#each recentRead as chapter}
         <div>
             <div class="bg-[#161218] flex border-b-[0.5px] border-gray-400 h-[85px]">
-                <div class="w-[100px] bg-[#D9D9D9]"></div>
+                <!-- <div class="w-[100px] bg-[#D9D9D9]"></div> -->
+                <img src={chapter.cover} alt={chapter.title} class="w-[85px] h-[85px] object-cover" />
                 <div class="flex justify-between items-center w-full pr-1">
                     <div class="items-center pl-5 justify-start">
-                        <p class="text-base"><b>{book.name}</b></p>
-                        <p class="text-base">Author</p>
+                        <p class="text-base"><b>{chapter.title}</b></p>
+                        <!-- <p class="text-base">Author</p> -->
                     </div>
                     <div class="justify-end">
-                        <p class="text-base">#37</p>
+                        <p class="text-base">#{chapter.number}</p>
                     </div>
                 </div>
             </div>
