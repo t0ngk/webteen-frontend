7f7615d6d221f8881ab38a45d09a77eaa57c4a59
T0NG.K <39180519+t0ngk@users.noreply.github.com>
2023-11-13 05:02:21 +0700

    ✨ : Login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,26 +1,27 @@
 <script>
-    let yes = false;
+    let username = null;
+    let password = null;
 </script>
 
 <div class="flex justify-center text-center items-center h-[95%] p-2">
-    <form class="flex flex-col space-y-5 w-10/12" action="/">
+    <div class="flex flex-col space-y-5 w-10/12">
         <h1 class="flex text-2xl mt-6 text-white font-bold">Log in</h1>
         
         <input class="shadow appearance-none border bg-transparent border-[#B4B4B4] rounded-3xl w-full p-3 text-[#B4B4B4] text-xs font-semibold leading-tight focus:outline-none focus:shadow-outline"
-        name="email" 
-        placeholder="Email / Username" 
+        name="email"
+        placeholder="Username"
         required
-        type="email"
-        
+        bind:value={username}
         />
         <input class="shadow appearance-none border bg-transparent border-[#B4B4B4] rounded-3xl w-full p-3 text-[#B4B4B4] text-xs font-semibold leading-tight focus:outline-none focus:shadow-outline"
         name="password"
         placeholder="Password"
         type="password"
         required
+        bind:value={password}
         />
 
-        <div class="flex font-medium text-sm px-2">
+        <!-- <div class="flex font-medium text-sm px-2">
             <input class="mr-2  bg-transparent checked:bg-yellow-500" 
                 type="checkbox" bind:checked={yes} />
             <p class="">
@@ -31,15 +32,38 @@
             >
                 Forgot password ?
             </a>
-        </div>
+        </div> -->
 
         <div></div>
         <button class=" text-black bg-[#F7B155] h-10 rounded-3xl font-bold text-sm tracking-wide disabled:bg-[#838383]"
-        disabled={!yes} on:click={() => console.log("login")}>
+        on:click={async () => {
+            const req = await fetch('http://localhost:8082/auth-service/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            });
+            if (req.ok) {
+                const {accessToken, refreshToken, isError, message} = await req.json();
+                if (isError) {
+                    alert(message);
+                } else {
+                    localStorage.setItem('accessToken', accessToken);
+                    localStorage.setItem('refreshToken', refreshToken);
+                    window.location.href = '/';
+                }
+            } else {
+                alert('Login failed');
+            }
+        }}>
             LOGIN
         </button>
 
-    </form>
+    </div>
 </div>
 <div class="text-center text-white font-medium text-sm mb-5">
     <p>
